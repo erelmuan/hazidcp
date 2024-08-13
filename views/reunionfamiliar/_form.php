0c01d886562f298a9343cd71f792203f4fc911a6
Elias Relmuan <eliasrelmuan@gmail.com>
2024-08-13 19:14:33 -0300

    cambio en script reunion familiar
    Se modifica el script que permitia no perder el scroll cuando se 
    desplegaba el segundo modal.

--- views/reunionfamiliar/_form.php
+++ views/reunionfamiliar/_form.php
@@ -126,12 +126,6 @@ $('.modal').on("hidden.bs.modal", function (e) {
         $('.modal-backdrop').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) - 10);
         $('body').addClass('modal-open');
     }
-}).on("show.bs.modal", function (e) {
-    if($('.modal:visible').length)
-    {
-        $('.modal-backdrop.in').first().css('z-index', parseInt($('.modal:visible').last().css('z-index')) + 10);
-        $(this).css('z-index', parseInt($('.modal-backdrop.in').first().css('z-index')) + 10);
-    }
 });
 function cerrarSoloSegundoModal() {
     $('#modal-plantilla').modal('hide');  // Solo cierra el segundo modal
